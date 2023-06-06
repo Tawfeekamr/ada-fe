68e3afb2e2a1b37dfd14b30b8bc3398c93246fe2
Tawfeeq Amro <tawfeekamr91@gmail.com>
2023-06-06 09:56:49 +0300

    init repo

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    "/api/v1",
+    "/api",
     createProxyMiddleware({
       target: "https://adaa.wisys.com.sa:3000",
       changeOrigin: true,
